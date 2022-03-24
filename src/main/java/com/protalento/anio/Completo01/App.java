906ec69f48c6f53c4a39a55d6fa153fd0ca92e5c
santiagobp24 <santiago83iti@gmail.com>
2022-03-23 20:05:25 -0500

    primero

---
+++ src/main/java/com/protalento/anio/Completo01/App.java
@@ -0,0 +1,16 @@
+package com.protalento.anio.Completo01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a, b;
+    	a = 2;
+    	b = 3;
+    	System.out.println(a+b);
+    }
+}
