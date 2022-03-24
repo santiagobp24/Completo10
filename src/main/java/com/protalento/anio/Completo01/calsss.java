fecb9dc87ff9a21f55dbcd3917b60c8c936224ab
santiagobp24 <santiago83iti@gmail.com>
2022-03-23 22:08:29 -0500

    quinto-feature1

---
+++ src/main/java/com/protalento/anio/Completo01/calsss.java
@@ -0,0 +1,12 @@
+package com.protalento.anio.Completo01;
+
+public class calsss {
+	public static void main( String[] args )
+    {
+    	int a, b;
+    	a = 2;
+    	b = 3;
+    	System.out.println(a+b*4);
+    }
+
+}
